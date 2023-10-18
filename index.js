65d7af2951efcde03621ce4aa9d4066912b47e86
Nick <ntroxler@ethz.ch>
2023-10-18 15:30:14 +0200

    add local node and express set up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { readFile } = require('fs').promises;
 const express = require('express');
 const { request } = require('http');
 const app = express();
-app.listen(8080, () => console.log("Up and running"))
+app.listen(process.env.PORT || 3000, () => console.log("Up and running at http://localhost:3000"))
 
 app.get('/', async (request, response) => {
     response.send( await readFile('./home.html', 'utf-8'));
